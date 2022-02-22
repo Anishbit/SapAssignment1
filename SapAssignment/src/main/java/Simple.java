ea8608d0c8f5f2f701b5356698892fdf6ff47c82
Anishbit <anish2504@gmail.com>
2022-02-22 17:46:46 +0530

    First Assignment_Commit

---
+++ SapAssignment/src/main/java/Simple.java
@@ -0,0 +1,13 @@
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName ="Anish";
+		String lastName ="Kumar";
+		
+		System.out.println("Hello "+firstName+" "+ lastName);
+
+	}
+
+}
